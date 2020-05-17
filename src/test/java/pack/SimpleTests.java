db6ba015e5da9373949785e803339a2d1a20ece4
Xeromant <in.e.t@yandex.ru>
2020-05-17 21:02:18 +0400

    test commit

---
+++ src/test/java/pack/SimpleTests.java
@@ -0,0 +1,22 @@
+package pack;
+
+import org.testng.annotations.Test;
+
+public class SimpleTests {
+    @Test
+    public void FirstTest()
+    {
+        System.out.println("1");
+    }
+    @Test
+    public void SecondTest()
+    {
+        System.out.println("2");
+    }
+
+    @Test
+    public void ThirdTest()
+    {
+        System.out.println("1");
+    }
+}
